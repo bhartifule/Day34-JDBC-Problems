7b02817fedb4e66e692d09c59746105789227413
Bharti <bharatifule10gmail.com>
2023-02-24 13:37:38 +0530

    Added: JDBC practice problem

--- main/java/com/bridglabz/mysql/Day34JDBC/EmpPayRollDBService.java
+++ main/java/com/bridglabz/mysql/Day34JDBC/EmpPayRollDBService.java
@@ -1,11 +1,16 @@
 package com.bridglabz.mysql.Day34JDBC;
 
+import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmpPayRollDBService {
-	  /**
+	 /**
      * Declaring Main Method Here
      * For Getting The Connection Of DataBase
      */
@@ -20,4 +25,26 @@ public class EmpPayRollDBService {
         return connection;
     }
 
+    /**
+     * retreiving the data from the database to get person salary
+     */
+    public List<EmpPayRollData> readData() {
+        String sql = "select * from employee_payroll ";
+        List<EmpPayRollData> employeePayRollList = new ArrayList<>();
+        try {
+            Connection connection = this.getConnection();
+            java.sql.Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            while (result.next()) {
+                int id = result.getInt("id");
+                String name = result.getString("name");
+                double basic_pay = result.getDouble("basic_pay");
+                LocalDate startDate = result.getDate("start_date").toLocalDate();
+                employeePayRollList.add(new EmpPayRollData(id, name, basic_pay, startDate));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employeePayRollList;
+    }
 }

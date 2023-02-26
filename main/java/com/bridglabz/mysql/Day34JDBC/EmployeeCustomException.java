4573b8068abd5a7a88a6bd7de131099d41dccaef
Bharti <bharatifule10gmail.com>
2023-02-26 23:22:16 +0530

    Added:JDBC problems

--- main/java/com/bridglabz/mysql/Day34JDBC/EmployeeCustomException.java
+++ main/java/com/bridglabz/mysql/Day34JDBC/EmployeeCustomException.java
@@ -1,9 +1,9 @@
 package com.bridglabz.mysql.Day34JDBC;
 
 public class EmployeeCustomException  extends Exception {
-    public EmployeeCustomException(String Invalid_Input)
-    {
-        super(Invalid_Input);
-    }
+	 public EmployeeCustomException(String Invalid_Input)
+	    {
+	        super(Invalid_Input);
+	    }
 
 }
--- main/java/com/bridglabz/mysql/Day34JDBC/EmployeePayroll.java
+++ main/java/com/bridglabz/mysql/Day34JDBC/EmployeePayroll.java
@@ -3,66 +3,26 @@ package com.bridglabz.mysql.Day34JDBC;
 	import java.util.Enumeration;
 
 	public class EmployeePayroll {
-	    static Connection con = null;
-
-	    public static void main(String[] args) throws EmployeeCustomException, SQLException {
-	        con = connected();
-	        reteriveData(con);
-	        updateData(con);
-	    }
-
-	    public static Connection connected() throws EmployeeCustomException {
-	        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false"; // declare JdbcURL
-	        String UserName = "root";
-	        String Password = "Root";
-	        Connection connection = null;
+		public static void preparedStatement(String name, double salary) {
 
 	        try {
-	            Class.forName("com.mysql.cj.jdbc.Driver"); // inbuilt method for Class.forName for loading driver
-	            System.out.println("Driver loaded");
+	            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root",
+	                    "Root");
+	            String s = "update employee_payroll set Salary = ? where Name=?";
+	            PreparedStatement ps = conn.prepareStatement(s);
+	            ps.setDouble(1, salary);
+	            ps.setString(2, name);
+	            ps.executeUpdate();
+	            boolean result = true;
 
-	        } catch (ClassNotFoundException e) {
-	            e.printStackTrace(); // for tracing the exception
-
-	        }
-	        listDrivers(); // static method calling
-	        try {
-	            System.out.println("Connecting to Database...:" + jdbcURL); // for loading the drive for connect
-	            connection = DriverManager.getConnection(jdbcURL, UserName, Password);
-	            System.out.println("connection successful" + connection);
-
-	        } catch (Exception e) {
+	        } catch (SQLException e) {
 	            e.printStackTrace();
 	        }
-	        return connection;
+
 	    }
 
-	    public static void reteriveData(Connection connection) throws EmployeeCustomException, SQLException {
-	        PreparedStatement ps = connection.prepareStatement("Select * from employee_payroll");
-	        ResultSet result = ps.executeQuery();
-	        while (result.next()) {
-	            System.out.print(result.getInt(1));
-	            System.out.print(" | ");
-	            System.out.print(result.getString(2));
-	            System.out.print(" | ");
-	            System.out.print(result.getString(3));
-	            System.out.print(" | ");
-	            System.out.println();
-	        }
-	    }
-	    public static void updateData(Connection connection) throws EmployeeCustomException, SQLException {
-	        PreparedStatement ps = connection.prepareStatement("update employee_payroll set salary = ? where id = ?;");
-	        ps.setDouble(1, 80000.00);
-	        ps.setInt(2, 2);
+	    public static void main(String[] args) throws SQLException {
+	        preparedStatement("Supriya",  300000.000);
 
-	        ps.executeUpdate();
-	        System.out.println("Update Successfully");
-	    }
-
-	    public static void listDrivers() {
-	        Enumeration<Driver> driverList = DriverManager.getDrivers();
-	        while (driverList.hasMoreElements()) { // static method for iteration.
-	            Driver driverClass = (Driver) driverList.nextElement();
-	            System.out.println(" " + driverClass.getClass().getName());
-	        }
+	    
 }}

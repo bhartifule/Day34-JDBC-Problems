52f686ee40cf031b4a6ffbec6a727a34f8745bfc
Bharti <bharatifule10gmail.com>
2023-02-26 23:16:33 +0530

    Added:JDBC problems

--- main/java/com/bridglabz/mysql/Day34JDBC/MainJDBC.java
+++
@@ -1,9 +0,0 @@
-package com.bridglabz.mysql.Day34JDBC;
-
-public class MainJDBC {
-public static void main(String[] args) {
-	System.out.println("Wellcome JDBC payroll Service");
-	 EmpPayRollDBService service = new EmpPayRollDBService();
-     System.out.println(service.readData());
-}
-}

c8f3ec1526348b43aba5060b4d8d4ffabb74ea4e
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-06-10 18:28:32 +0530

    EmployeeCustomException.java

--- main/java/com/bridglabz/mysql/Day34JDBC/EmployeeCustomException.java
+++ main/java/com/bridglabz/mysql/Day34JDBC/EmployeeCustomException.java
@@ -1,6 +1,7 @@
 package com.bridglabz.mysql.Day34JDBC;
 
-public class EmployeeCustomException  extends Exception {
+public class EmployeeCustomException  extends Exception 
+{
 	 public EmployeeCustomException(String Invalid_Input)
 	    {
 	        super(Invalid_Input);

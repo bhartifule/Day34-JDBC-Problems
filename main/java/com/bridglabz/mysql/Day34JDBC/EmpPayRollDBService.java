e7b0780cc366c9b77852c34f292926ee5a333ebd
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-06-10 18:20:47 +0530

    EmpPayRollDBService.java

--- main/java/com/bridglabz/mysql/Day34JDBC/EmpPayRollDBService.java
+++ main/java/com/bridglabz/mysql/Day34JDBC/EmpPayRollDBService.java
@@ -4,7 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class EmpPayRollDBService {
+public class EmpPayRollDBService
+{
 	  /**
      * Declaring Main Method Here
      * For Getting The Connection Of DataBase
